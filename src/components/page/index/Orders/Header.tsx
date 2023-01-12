3823bf0002bc2080a1c80cafc12c2122480e9bbe
ismailulgac-23 <82471789+ismailulgac-23@users.noreply.github.com>
2023-01-12 23:56:29 +0300

    Order header completed

--- src/components/page/index/Orders/Header.tsx
+++ src/components/page/index/Orders/Header.tsx
@@ -1,14 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { AppText } from "../../../ui";
 
 const Header = () => {
   return (
-    <View>
-      <Text>Header</Text>
+    <View style={styles.header}>
+      <AppText color="zinc" size="subtitle2" weight="500">
+        Siparişlerim
+      </AppText>
+      <AppText size="body3">Toplam 3 Adet</AppText>
     </View>
   );
 };
 
 export default Header;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+});
--- src/components/page/index/Orders/index.tsx
+++ src/components/page/index/Orders/index.tsx
@@ -2,11 +2,14 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Header from "./Header";
 import List from "./List";
+import { AppSizedBox } from "../../../ui";
+import { appSpaces } from "../../../../constants/app";
 
 const Orders = () => {
   return (
     <View>
       <Header />
+      <AppSizedBox height={appSpaces.xs} />
       <List />
     </View>
   );
