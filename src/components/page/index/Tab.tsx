9b3755fb07bd82e1bdb09fe9dc27e8c7d0834816
ismailulgac-23 <82471789+ismailulgac-23@users.noreply.github.com>
2023-01-12 23:44:12 +0300

    Tab completed

--- src/components/page/index/Tab.tsx
+++ src/components/page/index/Tab.tsx
@@ -1,14 +1,63 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { appColors } from "../../../constants/app";
+import { AppButton, AppText } from "../../ui";
+import { useState } from "react";
+
+type ITabItemProps = {
+  title: string;
+  selected: boolean;
+  onSelect: () => any;
+};
+
+const list = ["Tümü", "İptallerim", "İadelerim"];
+
+const TabItem = ({ title, selected, onSelect }: ITabItemProps) => {
+  const isSelected = selected;
+  return (
+    <AppButton
+      onPress={() => onSelect()}
+      color={isSelected ? "orange" : "bg"}
+      width={100}
+      radius={5}
+      height={40}
+    >
+      <AppText size="body2" color={isSelected ? "white" : "gray"}>
+        {title}
+      </AppText>
+    </AppButton>
+  );
+};
 
 const Tab = () => {
+  const [selected, setSelected] = useState(0);
   return (
-    <View>
-      <Text>Tab</Text>
+    <View style={styles.tab}>
+      {list.map((item, i) => (
+        <TabItem
+          title={item}
+          key={i}
+          selected={selected === i}
+          onSelect={() => setSelected(i)}
+        />
+      ))}
     </View>
   );
 };
 
 export default Tab;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  tab: {
+    width: 100 + "%",
+    marginTop: -40,
+    backgroundColor: appColors.white,
+    height: 70,
+    borderRadius: 5,
+    paddingVertical: 15,
+    paddingHorizontal: 13,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { getWindowSizes } from "../constants/helpers";
 import { FilterAndSearch, Header, Orders, Tab } from "../components/page/index";
+import { appColors, appSpaces } from "../constants/app";
+import { AppSizedBox } from "../components/ui";
 
 const IndexPage = () => {
   return (
@@ -9,9 +11,13 @@ const IndexPage = () => {
       <ScrollView style={{ ...getWindowSizes() }} horizontal={false}>
         <View style={styles.container}>
           <Header />
-          <Tab />
-          <FilterAndSearch />
-          <Orders />
+          <View style={styles.inner}>
+            <Tab />
+            <AppSizedBox height={appSpaces.xs} />
+            <FilterAndSearch />
+            <AppSizedBox height={appSpaces.xs} />
+            <Orders />
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -23,5 +29,9 @@ export default IndexPage;
 const styles = StyleSheet.create({
   container: {
     ...getWindowSizes(),
+    backgroundColor: "#f4f6fb",
+  },
+  inner: {
+    marginHorizontal: appSpaces.md,
   },
 });
